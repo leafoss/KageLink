c2a7c636fd10576c2d4c119290a6e9070a38d91d
leafoss <64222182+leafoss@users.noreply.github.com>
2026-07-26 19:55:26 -0300

    Route unified entry regression to v3.2.1

--- KageLink Installer/pc_agent/tests/test_unified_entry_v32_route.py
+++ KageLink Installer/pc_agent/tests/test_unified_entry_v32_route.py
@@ -5,16 +5,19 @@ import unittest
 import unified_entry
 from pc_agent import leafos_interpreter_v31 as v31
 from pc_agent import leafos_interpreter_v32 as v32
+from pc_agent import leafos_interpreter_v321 as v321
 
 
-class UnifiedEntryV32RouteTests(unittest.TestCase):
-    def test_packaged_entry_uses_v32_and_preserves_v31_module(self) -> None:
-        self.assertIs(unified_entry.LeafOSInterpreter, v32.LeafOSInterpreter)
-        self.assertIs(unified_entry.OllamaInterpreterProvider, v32.OllamaInterpreterProvider)
-        self.assertIs(unified_entry.launcher.LeafOSInterpreter, v32.LeafOSInterpreter)
-        self.assertIs(unified_entry.launcher.OllamaInterpreterProvider, v32.OllamaInterpreterProvider)
+class UnifiedEntryV321RouteTests(unittest.TestCase):
+    def test_packaged_entry_uses_v321_and_preserves_v31_v32_modules(self) -> None:
+        self.assertIs(unified_entry.LeafOSInterpreter, v321.LeafOSInterpreter)
+        self.assertIs(unified_entry.OllamaInterpreterProvider, v321.OllamaInterpreterProvider)
+        self.assertIs(unified_entry.launcher.LeafOSInterpreter, v321.LeafOSInterpreter)
+        self.assertIs(unified_entry.launcher.OllamaInterpreterProvider, v321.OllamaInterpreterProvider)
         self.assertEqual(v31.PROMPT_VERSION, "leafos-interpreter-v3.1")
         self.assertEqual(v32.PROMPT_VERSION, "leafos-interpreter-v3.2")
+        self.assertEqual(v321.PROMPT_VERSION, "leafos-interpreter-v3.2.1")
+        self.assertTrue(issubclass(v321.LeafOSInterpreter, v32.LeafOSInterpreter))
 
 
 if __name__ == "__main__":

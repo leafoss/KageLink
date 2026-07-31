997d4772398d0571d8b3c59e4a79a75df2af0ba9
leafoss <64222182+leafoss@users.noreply.github.com>
2026-07-31 20:09:35 -0300

    Export grid calibration model

--- navigation_lab/observer/__init__.py
+++ navigation_lab/observer/__init__.py
@@ -4,10 +4,12 @@ from .command_verifier import (
     PendingMovementCommand,
 )
 from .engine import MappingObserverEngine, MappingSnapshot
+from .grid_calibration import GridCalibration
 from .motion import MotionSample, PhaseCorrelationMotionEstimator
 from .tile_odometry import TileOdometry, TileOdometryUpdate
 
 __all__ = [
+    "GridCalibration",
     "MappingObserverEngine",
     "MappingSnapshot",
     "MotionSample",

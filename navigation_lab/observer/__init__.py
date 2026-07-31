6a7fc2f0d86ff38d2ec589dbc327daddb42c2c64
leafoss <64222182+leafoss@users.noreply.github.com>
2026-07-31 20:52:53 -0300

    Export semantic tile mapping components

--- navigation_lab/observer/__init__.py
+++ navigation_lab/observer/__init__.py
@@ -5,11 +5,23 @@ from .command_verifier import (
 )
 from .engine import MappingObserverEngine, MappingSnapshot
 from .grid_calibration import GridCalibration
+from .grid_cells import GridCellCrop, extract_grid_cells, grid_boundaries
 from .motion import MotionSample, PhaseCorrelationMotionEstimator
+from .tile_knowledge import (
+    TILE_CLASS_LABELS_PT_BR,
+    TileClass,
+    TileClassification,
+    TileExample,
+    TileFeatureExtractor,
+    TileKnowledgeBase,
+)
+from .tile_map_engine import ClassifiedGridCell, SemanticTileMapEngine, TileScanResult
 from .tile_odometry import TileOdometry, TileOdometryUpdate
 
 __all__ = [
+    "ClassifiedGridCell",
     "GridCalibration",
+    "GridCellCrop",
     "MappingObserverEngine",
     "MappingSnapshot",
     "MotionSample",
@@ -17,6 +29,16 @@ __all__ = [
     "MovementCommandVerifier",
     "PendingMovementCommand",
     "PhaseCorrelationMotionEstimator",
+    "SemanticTileMapEngine",
+    "TILE_CLASS_LABELS_PT_BR",
+    "TileClass",
+    "TileClassification",
+    "TileExample",
+    "TileFeatureExtractor",
+    "TileKnowledgeBase",
     "TileOdometry",
     "TileOdometryUpdate",
+    "TileScanResult",
+    "extract_grid_cells",
+    "grid_boundaries",
 ]

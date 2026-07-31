e9b00e337cff44f471936658e63a6d3d04bcf9ea
leafoss <64222182+leafoss@users.noreply.github.com>
2026-07-31 19:13:56 -0300

    Export input-confirmed mapping components

--- navigation_lab/observer/__init__.py
+++ navigation_lab/observer/__init__.py
@@ -1,3 +1,8 @@
+from .command_verifier import (
+    MovementCommandResolution,
+    MovementCommandVerifier,
+    PendingMovementCommand,
+)
 from .engine import MappingObserverEngine, MappingSnapshot
 from .motion import MotionSample, PhaseCorrelationMotionEstimator
 from .tile_odometry import TileOdometry, TileOdometryUpdate
@@ -6,6 +11,9 @@ __all__ = [
     "MappingObserverEngine",
     "MappingSnapshot",
     "MotionSample",
+    "MovementCommandResolution",
+    "MovementCommandVerifier",
+    "PendingMovementCommand",
     "PhaseCorrelationMotionEstimator",
     "TileOdometry",
     "TileOdometryUpdate",

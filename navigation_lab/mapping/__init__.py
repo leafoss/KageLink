052bc820bd552db21ca68fa680c9cfb37e886433
leafoss <64222182+leafoss@users.noreply.github.com>
2026-07-31 18:54:38 -0300

    Expose sparse tile map

--- navigation_lab/mapping/__init__.py
+++ navigation_lab/mapping/__init__.py
@@ -1,4 +1,13 @@
 from .grid import GridCell, OccupancyGrid
+from .sparse_map import SparseMapCell, SparseTileMap
 from .world import Region, RegionConnection, WorldGraph
 
-__all__ = ["GridCell", "OccupancyGrid", "Region", "RegionConnection", "WorldGraph"]
+__all__ = [
+    "GridCell",
+    "OccupancyGrid",
+    "SparseMapCell",
+    "SparseTileMap",
+    "Region",
+    "RegionConnection",
+    "WorldGraph",
+]
